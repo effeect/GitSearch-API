da091bb6a93deb74e11d06ed3b71fc9c6a0da473
Oliver Dimes <oliver.dimes3@gmail.com>
2025-12-08 17:50:26 +0900

    Intial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+// Simple Express Server that will handle the Github Requests
+const express = require("express");
+const cors = require("cors"); // For development, handle CORS
+
+const app = express();
+// MAKE CONFIGURABLE!!!
+const PORT = 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Example API Endpoint
+app.get("/api/data", (req, res) => {
+  res.json({ message: "Data fetched from Express API!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Express server running on http://localhost:${PORT}`);
+});
